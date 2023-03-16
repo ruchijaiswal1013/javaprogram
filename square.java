4f62cb5ee645d9b8d10b5a39b5f90b6046996522
ruchi jaiswal <118992151+ruchijaiswal1013@users.noreply.github.com>
2023-03-16 23:00:03 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Square{
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	//enter value which square you want
+	System.out.print("Enter no.=");
+	int n=sc.nextInt();
+	System.out.print("square of no. is ");
+	System.out.print(n*n);
+}
+}
